20111ba7ca6d1292e37fbcec343cda3d78a7bbd8
vercetti322 <vercettitommy322@gmail.com>
2024-03-24 21:46:27 +0530

    pixis-armin convo component made!

--- src/components/Pixis.vue
+++ src/components/Pixis.vue
@@ -1,11 +1,15 @@
 <script setup>
 import { ref, onMounted } from 'vue';
+import pixisLogo from '../assets/pixis-logo.jpg';
+import arminLogo from '../assets/armin-logo.webp';
 
 const transcript = ref(['sentence 1', 'sentence 2', 'sentence 3']);
 const currentIndex = ref(0);
 const typedText = ref('');
 const showNextButton = ref(false);
 const expanded = ref(false);
+const images = ref([pixisLogo, arminLogo]);
+const currentImage = ref(images.value[0]);
 
 let typingTimer;
 
@@ -22,7 +26,7 @@ const startTyping = () => {
                 showNextButton.value = false;
             }
         }
-    }, 50);
+    }, 75);
 };
 
 const toggleText = () => {
@@ -30,6 +34,7 @@ const toggleText = () => {
         clearInterval(typingTimer);
         currentIndex.value += 1;
         typedText.value = '';
+        currentImage.value = images.value[currentIndex.value % 2];
         startTyping();
     }
     expanded.value = true;
@@ -52,7 +57,7 @@ onMounted(() => {
 
 <template>
     <div class="pixis" :class="{ expanded: expanded }">
-        <img src="../assets/pixis-logo.jpg">
+        <img :src="currentImage">
         <p v-if="expanded">{{ typedText }}</p>
         <button v-if="showNextButton && expanded" @click="toggleText"><b>Next</b></button>
     </div>
@@ -77,13 +82,14 @@ onMounted(() => {
     border-radius: 5px;
     animation: fadeIn 1s ease-in-out forwards;
     animation-delay: 0.5s;
+    margin-left: 65px;
   }
   
   .pixis.expanded {
     position: relative;
     display: flex;
-    margin-left: 2px;
-    width: 97.5%;
+    margin-left: 65px;
+    width: 82%;
     display: flex;
     padding: 10px;
     background-color: #EADDCA;
@@ -93,13 +99,13 @@ onMounted(() => {
 }
   
   .pixis img {
-    max-height: 200px;
+    max-height: 125px;
     border-radius: 5px;
     border: 1px solid #C19A6B;
     margin-left: 30px;
     margin-top: 20px;
     margin-bottom: 20px;
-    margin-right: 130px;
+    margin-right: 90px;
     box-shadow: 0 2px 4px rgba(121, 74, 20, 0.75);
     cursor: pointer;
   }
@@ -107,6 +113,9 @@ onMounted(() => {
   .pixis p {
     margin: 0;
     padding: 0;
+    margin-top: 30px;
+    max-width: 390px;
+    word-wrap: break-word;
   }
   
   .pixis button {
@@ -118,7 +127,8 @@ onMounted(() => {
     background-color: wheat;
     border-radius: 2px;
     right: 0;
-    margin-right: 10px;
+    margin-right: 20px;
+    margin-top: 10px;
   }
   
   @keyframes fadeIn {

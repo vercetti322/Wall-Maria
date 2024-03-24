d50f0ba4200dc904d55aff3efa5514c4dbee7672
vercetti322 <vercettitommy322@gmail.com>
2024-03-24 10:52:29 +0530

    pixis div expansion made!

--- src/components/Pixis.vue
+++ src/components/Pixis.vue
@@ -1,41 +1,41 @@
 <script setup>
-    import { ref, onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 
-    const transcript = ref(['sentence 1', 'sentence 2', 'sentence 3']);
-    const currentIndex = ref(0);
-    const typedText = ref('');
-    const showNextButton = ref(false);
-    const expanded = ref(false); // New ref to control the expansion of the div
+const transcript = ref(['sentence 1', 'sentence 2', 'sentence 3']);
+const currentIndex = ref(0);
+const typedText = ref('');
+const showNextButton = ref(false);
+const expanded = ref(false);
 
-    let typingTimer;
+let typingTimer;
 
-    const startTyping = () => {
-        let index = 0;
-        typingTimer = setInterval(() => {
-            typedText.value += transcript.value[currentIndex.value][index];
-            index++;
-            if (index === transcript.value[currentIndex.value].length) {
-                clearInterval(typingTimer);
-                if (currentIndex.value < transcript.value.length - 1) {
-                    showNextButton.value = true;
-                } else {
-                    showNextButton.value = false;
-                }
-            }
-        }, 50);
-    };
-
-    const toggleText = () => {
-        if (currentIndex.value < transcript.value.length - 1) {
+const startTyping = () => {
+    let index = 0;
+    typingTimer = setInterval(() => {
+        typedText.value += transcript.value[currentIndex.value][index];
+        index++;
+        if (index === transcript.value[currentIndex.value].length) {
             clearInterval(typingTimer);
-            currentIndex.value += 1;
-            typedText.value = '';
-            startTyping();
+            if (currentIndex.value < transcript.value.length - 1) {
+                showNextButton.value = true;
+            } else {
+                showNextButton.value = false;
+            }
         }
-        expanded.value = true; // Expand the div when the button is clicked
-    };
+    }, 50);
+};
 
-    const expandDiv = () => {
+const toggleText = () => {
+    if (currentIndex.value < transcript.value.length - 1) {
+        clearInterval(typingTimer);
+        currentIndex.value += 1;
+        typedText.value = '';
+        startTyping();
+    }
+    expanded.value = true;
+};
+
+const expandDiv = () => {
     if (!expanded.value) {
         startTyping();
     }
@@ -52,11 +52,11 @@ onMounted(() => {
 
 <template>
     <div class="pixis" :class="{ expanded: expanded }">
-        <img src="../assets/pixis-logo.jpg" @click="expandDiv">
+        <img src="../assets/pixis-logo.jpg">
         <p v-if="expanded">{{ typedText }}</p>
         <button v-if="showNextButton && expanded" @click="toggleText"><b>Next</b></button>
     </div>
-  </template>
+</template>
 
 
 <style scoped>
@@ -68,8 +68,8 @@ onMounted(() => {
   .pixis {
     position: relative;
     opacity: 0;
-    transition: width 1s; /* Add transition for width */
-    width: 100px; /* Initial width */
+    transition: opacity 1s, width 0.5s; /* Add transition for opacity and width */
+    width: 100px;
     display: flex;
     padding: 10px;
     background-color: transparent;
@@ -78,7 +78,7 @@ onMounted(() => {
     animation: fadeIn 1s ease-in-out forwards;
     animation-delay: 0.5s;
   }
-
+  
   .pixis.expanded {
     position: relative;
     display: flex;
@@ -91,7 +91,7 @@ onMounted(() => {
     border-radius: 5px;
     box-shadow: 0 2px 4px rgba(121, 74, 20, 0.75);
 }
-
+  
   .pixis img {
     max-height: 200px;
     border-radius: 5px;
@@ -103,19 +103,12 @@ onMounted(() => {
     box-shadow: 0 2px 4px rgba(121, 74, 20, 0.75);
     cursor: pointer;
   }
-
-  .pixis #pixis-intro {
-    left: 0;
-    height: 100%;
-    margin-right: 80px;
-    width: 100%;
-    margin-top: 35px;
-    margin-bottom: 35px;
-    float: right;
-    padding: 5px;
-    font-size: 18px;
-  } 
-
+  
+  .pixis p {
+    margin: 0;
+    padding: 0;
+  }
+  
   .pixis button {
     position: absolute;
     color: rgb(84, 50, 6);
@@ -127,13 +120,22 @@ onMounted(() => {
     right: 0;
     margin-right: 10px;
   }
-
+  
   @keyframes fadeIn {
     from {
       opacity: 0;
     } 
     to {
       opacity: 1;
+    }
+  }
+
+  @keyframes fadeOut {
+    from {
+      width: 97.5%;
+    } 
+    to {
+      width: 100px;
     }
   }
 </style>

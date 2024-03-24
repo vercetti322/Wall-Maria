1e0b57d58f545e6874543183768232d38e3127e6
vercetti322 <vercettitommy322@gmail.com>
2024-03-25 01:34:09 +0530

    main page loading improved

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,12 @@
   import Header from './components/Header.vue';
 
   const Pixis = defineAsyncComponent(() => import('./components/Pixis.vue'));
+  const Div1 = defineAsyncComponent(() => import('./components/Div1.vue'));
+  const Div2 = defineAsyncComponent(() => import('./components/Div2.vue'));
 
   const showPixis = ref(false);
+  const showDiv1 = ref(false);
+  const showDiv2 = ref(false);
 
   onMounted(() => {
     window.addEventListener('scroll', () => {
@@ -13,7 +17,13 @@
       console.log('scrollPosition:', scrollPosition);
       console.log('pageHeight:', pageHeight);
       if (Math.abs(scrollPosition - pageHeight) < 1) {
-        showPixis.value = true;
+        if (!showPixis.value) {
+          showPixis.value = true;
+        } else if (!showDiv1.value) {
+          setTimeout(() => { showDiv1.value = true; }, 10000); // 20 seconds delay
+        } else if (!showDiv2.value) {
+          setTimeout(() => { showDiv2.value = true; }, 20000); // 20 seconds delay
+        }
         nextTick(() => {
           let lastScrollPosition = -1;
           const intervalId = setInterval(() => {
@@ -33,10 +43,12 @@
 <template>
   <Header/>
   <Pixis v-if="showPixis"/>
+  <Div1 v-if="showDiv1"/>
+  <Div2 v-if="showDiv2"/>
 </template>
 
 <style scoped>
-.pixis {
+.pixis, .div1, .div2 {
   margin-top: 30px;
   margin-bottom: 30px;
 }

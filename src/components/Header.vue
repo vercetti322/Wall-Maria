94dd31e9b1f5a4a3c1f77d5ba8605f85c8ab3b75
vercetti322 <vercettitommy322@gmail.com>
2024-03-24 08:33:37 +0530

    pixis info created!!

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,13 +7,12 @@
     <header>
       <div class="left-header">
         <h1>Wall Maria</h1>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam sapiente explicabo, provident atque assumenda
-            placeat at ut ipsam quis quia minus perspiciatis a quae unde architecto sed quibusdam nam odit magni libero
-            nesciunt? Quam dignissimos ducimus doloribus est eos. Culpa facere deserunt quaerat eligendi in, dolores
-            doloremque excepturi harum? Optio laborum sit qui repellat ab hic fuga dolorem aliquam veritatis cum, omnis
-            necessitatibus aperiam nihil molestiae ut exercitationem possimus nam impedit. Impedit fugiat quo ipsam omnis
-            quisquam aliquid voluptatem nostrum voluptatum, deserunt incidunt, facilis repudiandae, adipisci maxime molestiae
-            at! Illo labore in quae aspernatur inventore dolorem. Praesentium sunt explicabo nihil.
+          <p>
+            Paradis island is the last hope for humanity after the Titan genocide which occured 100 years ago
+            brigning humanity to near extinction. Now humanity is well-protected in the walls which enclose thousands
+            of houses, churches, palaces and gardens. The wall was created by Ymir (the Goddess for Eldians) by her mystical powers.
+            Wall Maria has been built carefully so as to utilise as less resources as possible yet covering all the boundary of humanity's 
+            habitat. Its only a matter of time till it's compromised....
           </p>
       </div>
       <div class="right-header">
@@ -29,11 +28,10 @@
     animation-delay: 0.5s;
     position: relative;
     display: flex;
-    background-color: aquamarine;
+    background-color: #C19A6B;
     background-image: url('../assets/wallpaper.jpg');
-    border: 2px solid black;
     background-size: cover;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
+    box-shadow: 0 2px 4px rgba(121, 74, 20, 0.75);
     border-radius: 10px;
   }
   
@@ -52,24 +50,32 @@
   }
   
   header .left-header h1 {
-    font-weight: 600;
+    font-weight: 800;
     font-size: 48px;
-    margin-left: 30px;
+    margin-left: 175px;
     font-family: 'Times New Roman', Times, serif;
   }
   
   header .left-header p {
     font-family: 'Times New Roman', Times, serif;
-    margin-left: 40px;
+    margin-left: 50px;
     font-weight: 400;
-    font-size: 18px;
+    margin-top: 15px;
+    font-size: 22px;
+    border: 2px solid rgb(19, 11, 2);
+    border-radius: 5px;
+    background-color: #C19A6B;
+    padding: 6px;
+    margin-right: 4px;
   }
   
   
   header .right-header img {
     width: 100%;
     height: 100%;
-    max-height: 350px;
+    margin-top: 35px;
+    margin-right: 10px;
+    max-height: 300px;
     min-height: 100px;
   }
   

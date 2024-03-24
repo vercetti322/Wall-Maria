f511e92019a7cff804ab495b203175e97df0bbd9
vercetti322 <vercettitommy322@gmail.com>
2024-03-24 10:25:00 +0530

    pixis div expansion made!

--- src/components/Pixis.vue
+++ src/components/Pixis.vue
@@ -1,17 +1,63 @@
+<script setup>
+    import { ref, onMounted } from 'vue';
 
+    const transcript = ref(['sentence 1', 'sentence 2', 'sentence 3']);
+    const currentIndex = ref(0);
+    const typedText = ref('');
+    const showNextButton = ref(false);
+    const expanded = ref(false); // New ref to control the expansion of the div
+
+    let typingTimer;
+
+    const startTyping = () => {
+        let index = 0;
+        typingTimer = setInterval(() => {
+            typedText.value += transcript.value[currentIndex.value][index];
+            index++;
+            if (index === transcript.value[currentIndex.value].length) {
+                clearInterval(typingTimer);
+                if (currentIndex.value < transcript.value.length - 1) {
+                    showNextButton.value = true;
+                } else {
+                    showNextButton.value = false;
+                }
+            }
+        }, 50);
+    };
+
+    const toggleText = () => {
+        if (currentIndex.value < transcript.value.length - 1) {
+            clearInterval(typingTimer);
+            currentIndex.value += 1;
+            typedText.value = '';
+            startTyping();
+        }
+        expanded.value = true; // Expand the div when the button is clicked
+    };
+
+    const expandDiv = () => {
+    if (!expanded.value) {
+        startTyping();
+    }
+    expanded.value = true;
+    // Remove event listener after the div is expanded
+    document.querySelector('.pixis img').removeEventListener('click', expandDiv);
+};
+
+onMounted(() => {
+    // Add event listener to the image to expand the div
+    document.querySelector('.pixis img').addEventListener('click', expandDiv);
+});
+</script>
 
 <template>
-  <Header/>
-  <div class="pixis">
-      <img src="../assets/pixis-logo.jpg">
-      <p id="pixis-intro">Lorem ipsum do
-        lor, sit amet consectetur adipis
-        icing elit. Nesciunt hic nam ani
-        
-        mi suscipit explicabo alias rerum at, vitae recusandae doloremque unde molestiae eligendi, ipsa sapiente, ea repellat sit necessitatibus non. Numquam molestiae cum porro, nisi consequuntur exercitationem saepe omnis eligendi, nam magnam cumque sapiente sint ea nesciunt repudiandae expedita eos.
-      </p>
-  </div>
-</template>
+    <div class="pixis" :class="{ expanded: expanded }">
+        <img src="../assets/pixis-logo.jpg" @click="expandDiv">
+        <p v-if="expanded">{{ typedText }}</p>
+        <button v-if="showNextButton && expanded" @click="toggleText"><b>Next</b></button>
+    </div>
+  </template>
+
 
 <style scoped>
 
@@ -20,16 +66,31 @@
   }
 
   .pixis {
+    position: relative;
     opacity: 0;
+    transition: width 1s; /* Add transition for width */
+    width: 100px; /* Initial width */
+    display: flex;
+    padding: 10px;
+    background-color: transparent;
+    border: none;
+    border-radius: 5px;
+    animation: fadeIn 1s ease-in-out forwards;
+    animation-delay: 0.5s;
+  }
+
+  .pixis.expanded {
+    position: relative;
+    display: flex;
+    margin-left: 2px;
+    width: 97.5%;
     display: flex;
     padding: 10px;
     background-color: #EADDCA;
     border: none;
     border-radius: 5px;
-    animation: fadeIn 1s ease-in-out forwards;
-    animation-delay: 0.5s;
     box-shadow: 0 2px 4px rgba(121, 74, 20, 0.75);
-  }
+}
 
   .pixis img {
     max-height: 200px;
@@ -40,6 +101,7 @@
     margin-bottom: 20px;
     margin-right: 130px;
     box-shadow: 0 2px 4px rgba(121, 74, 20, 0.75);
+    cursor: pointer;
   }
 
   .pixis #pixis-intro {
@@ -53,6 +115,18 @@
     padding: 5px;
     font-size: 18px;
   } 
+
+  .pixis button {
+    position: absolute;
+    color: rgb(84, 50, 6);
+    height: 25px;
+    border: 1px solid rgb(84, 50, 6);
+    padding: 5px;
+    background-color: wheat;
+    border-radius: 2px;
+    right: 0;
+    margin-right: 10px;
+  }
 
   @keyframes fadeIn {
     from {
